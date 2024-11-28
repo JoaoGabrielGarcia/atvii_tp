5d49c5cd0c405b00017caa14a7a5299b69f77e90
JoaoGabrielGarcia <joaog.oliveiragarcia@gmail.com>
2024-11-27 22:30:43 -0300

    Ajustes sidebar

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -3,7 +3,7 @@ import React from "react";
 class Header extends React.Component {
     render() {
         return (
-            <header className="w-full bg-gray-800 text-black p-4">
+            <header className="w-full text-black p-4">
                 <h1 className="text-xl">Sistema de Gestão</h1>
             </header>
         );
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,32 +1,77 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import { Sidebar } from "flowbite-react";
-import { HiShoppingBag, HiTable, HiUser } from "react-icons/hi";
-
+import { HiShoppingBag, HiTable, HiUser, HiMenu } from "react-icons/hi";
 
 export default class Navbar extends React.Component {
+   constructor(props) {
+      super(props);
+      this.state = {
+         isSidebarOpen: false,
+      };
+
+      this.toggleSidebar = this.toggleSidebar.bind(this);
+      this.closeSidebar = this.closeSidebar.bind(this);
+   }
+
+   toggleSidebar() {
+      this.setState((prevState) => ({
+         isSidebarOpen: !prevState.isSidebarOpen,
+      }));
+   }
+
+   closeSidebar() {
+      this.setState({ isSidebarOpen: false });
+   }
+
    render() {
+      const { isSidebarOpen } = this.state;
+
       return (
-         <aside className="h-20 bg-red-500">
-            <nav className="w-64 h-screen  text-white ">
-            <Sidebar className="bg-lime-500">
-               <Sidebar.Items>
-                  <Sidebar.ItemGroup>
-                     <Sidebar.Item icon={HiUser}>
-                     <Link to="/clientes">Clientes</Link>
-                     </Sidebar.Item>
-                     <Sidebar.Item icon={HiShoppingBag}>
-                     <Link to="/produtos">Produtos</Link>
-                     </Sidebar.Item>
-                     <Sidebar.Item icon={HiTable}>
-                     <Link to="/servicos">Serviços</Link>
-                     </Sidebar.Item>
-                  </Sidebar.ItemGroup>
-               </Sidebar.Items>
-            </Sidebar>
-         </nav>
-         </aside>
-         
+         <div className="relative">
+            {/* Botão para abrir/fechar a Sidebar em dispositivos móveis */}
+            <button
+               className={`fixed top-4 left-4 p-2 text-white bg-slate-300 rounded-xl z-50 lg:hidden ${
+                  isSidebarOpen ? "hidden" : "block"
+               }`}
+               onClick={this.toggleSidebar}
+            >
+               <HiMenu size={24} />
+            </button>
+
+            {/* Sidebar */}
+            <aside
+               className={`fixed top-0 left-0 h-full transition-transform transform ${
+                  isSidebarOpen ? "translate-x-0" : "-translate-x-full"
+               } lg:translate-x-0 lg:static lg:h-auto z-40`}
+            >
+               <nav className="w-64 h-screen text-white">
+                  <Sidebar>
+                     <Sidebar.Items>
+                        <Sidebar.ItemGroup>
+                           <Sidebar.Item icon={HiUser}>
+                              <Link to="/clientes">Clientes</Link>
+                           </Sidebar.Item>
+                           <Sidebar.Item icon={HiShoppingBag}>
+                              <Link to="/produtos">Produtos</Link>
+                           </Sidebar.Item>
+                           <Sidebar.Item icon={HiTable}>
+                              <Link to="/servicos">Serviços</Link>
+                           </Sidebar.Item>
+                        </Sidebar.ItemGroup>
+                     </Sidebar.Items>
+                  </Sidebar>
+               </nav>
+            </aside>
+
+            {/* Overlay para fechar a Sidebar em dispositivos móveis */}
+            {isSidebarOpen && (
+               <div
+                  className="fixed inset-0 bg-black bg-opacity-50 z-30 lg:hidden"
+                  onClick={this.closeSidebar}
+               ></div>
+            )}
+         </div>
       );
    }
 }
